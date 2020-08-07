4f38fd9c7574053312b05793345dee7157230130
Tina-Lightning <c.atherholt@gmail.com>
2020-08-07 17:23:45 -0400

    added more files

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = "ab0daf48e5c9f6b2513a909b48005ede";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const IMAGE_URL = "http://image.tmdb.org/t/p/";
