fe6f8dee8ec12caceb652084c0ab8eb20db2525a
Tina-Lightning <c.atherholt@gmail.com>
2020-08-26 09:54:47 -0400

    made progress on individual movie page

--- client/src/components/views/LandingPage/Sections/GridCard.js
+++ client/src/components/views/LandingPage/Sections/GridCard.js
@@ -3,15 +3,22 @@ import { Col } from "antd";
 //const { Title } = Typography;
 
 function GridCard(props) {
-    return (
-        <Col lg={6} md={8} xs={24}>
-            <div style={{ position:"relative"}}>
-                <a href={`/movie/${props.movieId}`}>
-                    <img style={{ width:"100%", height:"320px"}} alt="img" src={props.image} />
-                </a>
-            </div>
-        </Col>
-    )
+
+    if(props.actor) {
+        return(
+            <div></div>
+        )
+    } else {
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position:"relative"}}>
+                    <a href={`/movie/${props.movieId}`}>
+                        <img style={{ width:"100%", height:"320px"}} alt="img" src={props.image} />
+                    </a>
+                </div>
+            </Col>
+        )
+    }
 }
 
 export default GridCard;
