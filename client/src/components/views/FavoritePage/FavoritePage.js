d14b5531ae9b1a05d991290f0ab8cf5f407819b0
Tina-Lightning <c.atherholt@gmail.com>
2020-09-01 09:54:23 -0400

    kind of finished the app

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,29 +1,65 @@
 import React, { useEffect, useState } from "react"
 import "./favorite.css"
 import Axios from "axios"
+import  { Popover } from "antd";
+import { IMAGE_URL } from "../../Config";
 
 function FavoritePage() {
 
     const variables = { userFrom: localStorage.getItem("userId") }
     
-    const [FavoriteMovie, setFavoriteMovie] = useState([])
+    const [FavoriteMovies, setFavoriteMovies] = useState([])
 
     useEffect(() => {
+        fetchFavoritedMovies();
+    }, [])
+
+    const fetchFavoritedMovies = () => {
         Axios.post("/api/favorite/getFavoriteMovie", variables)
         .then(response => {
             if(response.data.success) {
-                setFavoriteMovie(response.data.favorites)
+                setFavoriteMovies(response.data.favorites)
             } else {
                 alert("Failed to get favorite movies")
             }
         })
-    }, [])
+    }
 
-    const renderTableBody = FavoriteMovie.map((movie, index) => {
+    const onClickRemove = (movieId) => {
+
+        const variable = {
+            movieId: movieId,
+            userFrom: localStorage.getItem("userId")
+        }
+
+        Axios.post("/api/favorite/removeFromFavorite", variable)
+            .then(response => {
+                if(response.data.success){
+                    fetchFavoritedMovies();
+                } else {
+                    alert("Failed to remove from Favorite")
+                }
+            })
+    }
+
+    const renderTableBody = FavoriteMovies.map((movie, index) => {
+
+        const content = (
+            <div>
+                {movie.moviePost ?
+                <img src={`${IMAGE_URL}w500${movie.moviePost}`} alt="moviePost" />
+                :
+                "No Image"
+                }
+            </div>
+        )
+
         return <tr>
+            <Popover content={content} title={`${movie.movieTitle}`}>
             <td>{movie.movieTitle}</td>
+            </Popover>
             <td>{movie.movieRunTime} mins</td>
-            <td><button>Remove from Favorites</button></td>
+            <td><button onClick={()=>onClickRemove(movie.movieId)}>Remove from Favorites</button></td>
         </tr>
     })
 
